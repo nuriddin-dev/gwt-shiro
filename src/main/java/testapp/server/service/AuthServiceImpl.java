a87b8b7270f178821c2c970b59d1684b86c6aa89
Nuriddin Baideuov <baideuov@gmail.com>
2015-03-03 11:27:34 +0600

    shiro enabled

--- src/main/java/testapp/server/service/AuthServiceImpl.java
+++ src/main/java/testapp/server/service/AuthServiceImpl.java
@@ -1,17 +1,31 @@
 package testapp.server.service;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import testapp.client.service.AuthService;
 
 
 public class AuthServiceImpl extends RemoteServiceServlet implements AuthService {
     @Override
     public boolean isLoggedIn() {
-        return false;
+        Subject currentUser = SecurityUtils.getSubject();
+        return currentUser.isAuthenticated();
     }
 
     @Override
     public boolean login(String userName, String password, boolean rememberMe) {
+        UsernamePasswordToken token = new UsernamePasswordToken(userName, password);
+        token.setRememberMe(rememberMe);
+
+        Subject currentUser = SecurityUtils.getSubject();
+        try {
+            currentUser.login(token);
+        } catch (AuthenticationException ae) {
+            return false;
+        }
         return true;
     }
 
